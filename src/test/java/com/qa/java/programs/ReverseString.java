507f9cfde3f6310bcfe0b0cf5bb7f0078d3ad453
Naveen Reddy <39487831+NaveenAutomationHub@users.noreply.github.com>
2023-07-22 17:03:01 +0530

    Update ReverseString.java

--- src/test/java/com/qa/java/programs/ReverseString.java
+++ src/test/java/com/qa/java/programs/ReverseString.java
@@ -7,7 +7,7 @@ public class ReverseString {
 		StringBuilder str = sb.reverse();		
 		System.out.println(str);
 
-		String input1= "Naveen Harisha";
+		String input1= "Naveen Har";
 		char[] ch = input1.toCharArray();
 		for(int reverse=ch.length-1;reverse>=0;reverse--){	
 			System.out.print(ch[reverse]);
